7348912d1a0a2cdaeae0ce186ace1402864ff708
Vikram <59924856+justVikram@users.noreply.github.com>
2022-04-14 18:23:01 +0530

    add script to upload files to Google Drive using gdrive

---
+++ upload.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+echo "Enter path to directory you wish to backup:"
+read -r path
+echo "Enter name of backup file:"
+read -r file
+tar -czvf $file.tar.gz $path > /dev/null
+echo "Backup complete!"
+echo "Uploading to Google Drive..."
+
+# Get parent directory's ID from .env file
+filename='.env'
+while read -r line
+do
+    key=$line
+done < $filename
+
+gdrive upload --delete -p $key $file.tar.gz
+if [ $? -eq 0 ]
+then
+    
+    echo "Upload complete"
+else
+    echo "Backup failed!"
+fi
