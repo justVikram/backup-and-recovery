94d4bf22f1c3b2ef9dae95d756dc6946742a6c6e
Vikram <59924856+justVikram@users.noreply.github.com>
2022-04-14 18:21:30 +0530

    add script to download files using gdrive utility

---
+++ download.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+echo "Now downloading Backups folder to your local machine..."
+
+# Get parent directory's ID from .env file
+filename='.env'
+while read -r line
+do
+    key=$line
+done < $filename
+
+gdrive download --recursive $key
+echo "Download complete!"
